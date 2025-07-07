27f5c395ab332fc372d566fb30c7058174630750
Benjamin <benjamin.bats.dev@gmail.com>
2025-07-07 12:38:14 +0200

    Fixing names

---
+++ src/contants/symbol-conversions.constants.ts
@@ -0,0 +1,14 @@
+export const symboles = [
+  {
+    initial: "&lt;",
+    converted: "<"
+  },
+  {
+    initial: "&gt;",
+    converted: ">"
+  },
+  {
+    initial: "&nbsp;",
+    converted: " "
+  },
+]
